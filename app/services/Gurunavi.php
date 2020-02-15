43d24bec4462904223dfa5048a1a48645527663c
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-02-15 18:42:51 +0900

    gnavi

---
+++ app/services/Gurunavi.php
@@ -0,0 +1,24 @@
+<?php
+
+  namespace App\Services;
+
+  use GuzzleHttp\Client;
+
+  class Gurunavi
+  {
+    public function searchRestaurants($word)
+    {
+      // -- ここから追加
+          $client = new Client();
+          $response = $client
+              ->get('https://api.gnavi.co.jp/RestSearchAPI/v3/', [
+                  'query' => [
+                      'keyid' => env('GURUNAVI_ACCESS_KEY'),
+                      'freeword' => str_replace(' ', ',', $word),
+                  ],
+              ]);
+              
+          return json_decode($response->getBody()->getContents(), true); 
+           
+    }
+  }
