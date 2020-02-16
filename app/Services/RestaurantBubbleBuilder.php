399f7ec7b345ef8a3afdb646d0b2c8e30a7eed84
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-02-16 09:25:25 +0900

    setter

--- app/Services/RestaurantBubbleBuilder.php
+++ app/Services/RestaurantBubbleBuilder.php
@@ -16,4 +16,23 @@ class RestaurantBubbleBuilder implements ContainerBuilder
   private $longitude;
   private $phoneNumber;
   private $restaurantUrl;
+
+  public static function builder(): RestaurantBubbleBuileder
+  {
+    return new self();
+  }
+
+  public function setContents(array $restaurant): void
+  {
+    $this->imageUrl = Arr::get($restaurant, 'image_url.shop_image1', null);
+    $this->name = Arr::get($restaurant, 'name', null);
+    $this->closeestStation = Arr::get($restaurant, 'access.station', null);
+    $this->minutesByFoot = Arr::get($restaurant, 'access.walk', null);
+    $this->category = Arr::get($restaurant, 'budget', null);
+    $this->latitude = Arr::get($restaurant, 'latitude', null);
+    $this->longitude = Arr::get($restaurant, 'longitude', null);
+    $this->phoneNumber = Arr::get($restaurant, 'tel', null);
+    $this->restaurantUrl = Arr::get($restaurant, 'url , null');
+  }
+
 }
